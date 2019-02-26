507df56a64e020ad6a32ab402b30a0e8ce69bc27
Matt Eding <matteding@gmail.com>
2019-02-25 20:16:07 -0800

    refactored spelling_mp

--- spelling_mp.py
+++ spelling_mp.py
@@ -1,4 +1,3 @@
-import itertools
 import logging
 import multiprocessing
 import pathlib
@@ -8,45 +7,62 @@ import pandas as pd
 from textblob import TextBlob
 
 
-name = input('Input data name: ')
-start = int(input('Input start index: '))
-stop = input('Input stop index: ')
-if not stop:
-    stop = None
-else:
-    stop = int(stop)
+#: User Input Prompts
+NAME = input('Input data name: ')
+START = int(input('Input start index: '))
+try:
+    STOP = int(input('Input stop index: '))
+except Exception:
+    STOP = None
 
 
+
+#: Directory Paths
 data = pathlib.Path.cwd() / 'data'
 pkls = data / 'pkls'
 npys = data / 'npys'
+logs = data / 'logs'
 
 
 #: Launch Logger
-log_file = data / 'logs' / f'{name}_corrections.log'
+log_file = logs / f'{NAME}_corrections.log'
 log_file.touch()
 fmt = '{name} - {asctime} - {levelname} - Message: {message}'
-logging.basicConfig(filename=log_file, level=logging.INFO, style='{', format=fmt)
+logging.basicConfig(filename=log_file, 
+                    level=logging.INFO, 
+                    style='{', 
+                    format=fmt)
 logger = logging.getLogger(__name__)
 
-#: Load essay array
-pkl = pkls / f'{name}.pkl'
-df = pd.read_pickle(pkl)        
-essays = df['essay'][start:stop]
 
-#: Load correction array
-npy = npys / f'{name}_corrections.npy'
+#: Load Essay Array
+pkl = pkls / f'{NAME}.pkl'
+df = pd.read_pickle(pkl)        
+essays = df['essay'][START:STOP]
+
+
+#: Load Correction Array
+npy = npys / f'{NAME}_corrections.npy'
 if npy.exists():
     arr = np.load(npy)
 else:
     arr = np.empty(len(df['essay']), dtype=object)
 
 
-cycle = itertools.cycle(range(10))
-next(cycle) # don't np.save immediately
-
-
-def correct(i_essay, *, _cycle=cycle):
+def correct(i_essay):
+    """Correct the ith essay spelling.
+    
+    Parameters
+    ----------
+    i_essay : (int, str)
+        Pair containing index and string.
+    
+    Returns
+    -------
+    corr : str
+        String of corrections. 
+    """
+    
     i, essay = i_essay
     try:
         corr = str(TextBlob(essay).correct())
@@ -54,12 +70,23 @@ def correct(i_essay, *, _cycle=cycle):
         logger.exception(exc)
         raise
     else:
-        logger.info(f'Corrected Index: {name} @ {i}')
+        logger.info(f'Corrected Index: {NAME} @ {i}')
     return i, corr
 
 
-def correct_range(name, start=0, stop=None):
-    logger.info(f'Start Index: {name} @ {start}')
+def correct_range(start=0, stop=None):
+    """Correct all essays in the given range from start to stop.
+    
+    Parameters
+    ----------
+    start : int, optional
+        Slice start index.
+    
+    stop : int, optional
+        Slice stop index.
+    """
+    
+    logger.info(f'Start Index: {NAME} @ {start}')
     
     with multiprocessing.Pool() as pool:
         i_corrs = pool.map(correct, enumerate(essays, start=start))
@@ -67,8 +94,8 @@ def correct_range(name, start=0, stop=None):
     idxs, corrs = map(list, zip(*i_corrs))
     arr[idxs] = corrs
     np.save(npy, arr)
-    logger.info(f'Stop Index: {name} @ {stop}')
+    logger.info(f'Stop Index: {NAME} @ {stop}')
 
     
 if __name__ == '__main__':
-    correct_range(name, start)
+    correct_range(START, STOP)
