4fe867e14cfe4745d2e2d36ba276da966d3c430b
Matt Eding <matteding@gmail.com>
2019-02-25 19:19:08 -0800

    fixed bug with mutable object assignment with multiproc

--- spelling_mp.py
+++ spelling_mp.py
@@ -10,6 +10,11 @@ from textblob import TextBlob
 
 name = input('Input data name: ')
 start = int(input('Input start index: '))
+stop = input('Input stop index: ')
+if not stop:
+    stop = None
+else:
+    stop = int(stop)
 
 
 data = pathlib.Path.cwd() / 'data'
@@ -17,52 +22,53 @@ pkls = data / 'pkls'
 npys = data / 'npys'
 
 
+#: Launch Logger
 log_file = data / 'logs' / f'{name}_corrections.log'
 log_file.touch()
 fmt = '{name} - {asctime} - {levelname} - Message: {message}'
 logging.basicConfig(filename=log_file, level=logging.INFO, style='{', format=fmt)
 logger = logging.getLogger(__name__)
 
+#: Load essay array
+pkl = pkls / f'{name}.pkl'
+df = pd.read_pickle(pkl)        
+essays = df['essay'][start:stop]
 
+#: Load correction array
 npy = npys / f'{name}_corrections.npy'
 if npy.exists():
     arr = np.load(npy)
 else:
-    arr = np.empty(len(essays), dtype=object)
+    arr = np.empty(len(df['essay']), dtype=object)
 
 
-cycle = itertools.cycle(range(50))
+cycle = itertools.cycle(range(10))
 next(cycle) # don't np.save immediately
 
 
 def correct(i_essay, *, _cycle=cycle):
     i, essay = i_essay
-    corr = str(TextBlob(essay).correct())
     try:
-        arr[i] = corr
-        logger.info(f'Corrected Index: {name} @ {i}')
+        corr = str(TextBlob(essay).correct())
     except BaseException as exc:
         logger.exception(exc)
         raise
-    finally:
-        if not next(_cycle):
-            np.save(npy, arr)
-            logger.info(f'Saved Corrections: {name} @ {i-1}...(mp)')
+    else:
+        logger.info(f'Corrected Index: {name} @ {i}')
+    return i, corr
 
 
 def correct_range(name, start=0, stop=None):
-    pkl = pkls / f'{name}.pkl'
-
     logger.info(f'Start Index: {name} @ {start}')
     
-    df = pd.read_pickle(pkl)        
-    essays = df['essay'][start:stop]
-
     with multiprocessing.Pool() as pool:
-        corrs = pool.map(correct, enumerate(essays, start=start))
+        i_corrs = pool.map(correct, enumerate(essays, start=start))
     
+    idxs, corrs = map(list, zip(*i_corrs))
+    arr[idxs] = corrs
+    np.save(npy, arr)
     logger.info(f'Stop Index: {name} @ {stop}')
-    
 
+    
 if __name__ == '__main__':
     correct_range(name, start)
