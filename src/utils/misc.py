f24a1a0ded6d284da3ca890eafb3a40a67dc528b
Matt Eding <matteding@gmail.com>
2019-03-04 22:18:12 -0800

    flask app docstrings

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -13,46 +13,6 @@ npys = data / 'npys'
 pkls = data / 'pkls'
 
 
-def public(obj):
-    """Return list of public attributes of obj.
-
-    Parameters
-    ----------
-    obj : object
-        Object to inspect public attributes of.
-
-    Returns
-    -------
-    attrs : list of strings
-        List of public attributes of object
-    """
-
-    attrs = [attr for attr in dir(obj) if not attr.startswith('_')]
-    return attrs
-
-
-def stream_extract(zipfile, *file_indices):
-    """Extract files without saving them to disk.
-
-    Parameters
-    ----------
-    zipfile : ZipFile
-        ZipFile object to extract info from.
-    *file_indices : int
-        Indices of the files to extract from zipfile.
-
-    Returns
-    -------
-    files: generator of files
-        Generator yielding extracted files.
-    """
-
-    files = (zipfile.filelist[idx] for idx in file_indices)
-    files = (zipfile.read(file) for file in files)
-    files = (io.BytesIO(file) for file in files)
-    return files
-
-
 def get_prompt(readme):                                 # Move to feature_util?
     """Return prompt of essay from a docx file.
 
@@ -145,3 +105,43 @@ def merge_features(pkls_path, name):
     dfs = [difficulty_level_df, error_ratio_df, grade_level_df, pos_df,
            prompt_similarity_df, sentiment_df, words_df, percent_df]
     return pd.concat(dfs, axis=1)
+
+
+def public(obj):
+    """Return list of public attributes of obj.
+
+    Parameters
+    ----------
+    obj : object
+        Object to inspect public attributes of.
+
+    Returns
+    -------
+    attrs : list of strings
+        List of public attributes of object
+    """
+
+    attrs = [attr for attr in dir(obj) if not attr.startswith('_')]
+    return attrs
+
+
+def stream_extract(zipfile, *file_indices):
+    """Extract files without saving them to disk.
+
+    Parameters
+    ----------
+    zipfile : ZipFile
+        ZipFile object to extract info from.
+    *file_indices : int
+        Indices of the files to extract from zipfile.
+
+    Returns
+    -------
+    files: generator of files
+        Generator yielding extracted files.
+    """
+
+    files = (zipfile.filelist[idx] for idx in file_indices)
+    files = (zipfile.read(file) for file in files)
+    files = (io.BytesIO(file) for file in files)
+    return files
